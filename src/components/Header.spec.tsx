610fccc02e009b1c608e9799c1a61f481cfcc2df
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-04-10 10:30:17 -0300

    Delete Header.spec.tsx

--- src/components/Header.spec.tsx
+++
@@ -1,9 +0,0 @@
-// import { render } from '@testing-library/react'
-// import { Header } from './Header'
-
-describe('Test header', () => {
-  it('test render', () => {
-    // render(<Header />)
-    expect(1).toBe(1)
-  })
-})
